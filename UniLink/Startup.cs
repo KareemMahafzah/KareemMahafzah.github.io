d2c76267b542332a4f5405401b8defe41329afef
KareemMahafzah <KareemMahafzah@outlook.com>
2023-05-17 20:04:26 +0300

    1

---
+++ UniLink/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniLink.Startup))]
+namespace UniLink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
